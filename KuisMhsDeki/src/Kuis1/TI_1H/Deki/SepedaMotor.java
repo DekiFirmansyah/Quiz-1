b7d24d0100b46f83b67ca2ef3ac24abaad1589df
DekiFirmansyah <79009695+DekiFirmansyah@users.noreply.github.com>
2021-03-06 09:07:33 +0700

    Add files via upload
    Pengumpulan Kuis 1

---
+++ KuisMhsDeki/src/Kuis1/TI_1H/Deki/SepedaMotor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kuis1.TI_1H.Deki;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class SepedaMotor {
+    
+}
